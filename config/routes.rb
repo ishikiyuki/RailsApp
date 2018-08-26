6eff319fe74481548faa367c047a6bb567be22c5
ishikiyuki <yuki.ishiki@gmail.com>
2018-08-26 15:49:00 +0900

    RailsApp first commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'cards/index'
+  get 'cards', to: 'cards#index'
+
+  get 'cards/add'
+  post 'cards/add'
+
+  get 'cards/:id', to: 'cards#show'
+
+  get 'cards/edit/:id', to: 'cards#edit'
+  patch 'cards/edit/:id', to: 'cards#edit'
+
+  get 'cards/delete/:id', to: 'cards#delete'
+end
